367550c904cf7a56772d8c7328b3c7e4773f7b62
MariamK <102153226+MariamKakushadze@users.noreply.github.com>
2023-02-24 19:17:08 +0400

    style: add padding to h1"

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import headerImg from "../../assets/headerImg.png";
+import HeaderBtn from "./HeaderBtn";
 
 const Header = (props) => {
   return (
     <>
       <header className="fixed top-0 left-0 w-[100%] h-20 bg-[#b55c6b] text-white flex justify-between align-middle content-center flex-wrap text-center px-[10%] z-10 shadow-sm shadow-pink-300">
-        <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <h1 className="pt-2">ReactMeals</h1>
+        <HeaderBtn />
       </header>
       <div className="w-[100%] h-80 z-0 overflow-hidden">
         <img
